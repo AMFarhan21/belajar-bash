af3daeb03b6c65ec539fb4001640b0c40c85b68b
AMFarhan21 <AMFarhan2102@gmail.com>
2025-08-22 22:20:07 +0800

    first init

---
+++ belajarBash.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+SOURCE_DIR="."
+BACKUP_DIR="/f/HACKTIV8/Phase 0/Week 1/Day 4/backup"
+
+mkdir -p "$BACKUP_DIR"
+
+for file in $(find "$SOURCE_DIR" -type f -size +5M); do
+	filename=$(basename "$file")
+	tar -czf "$BACKUP_DIR/${filename}.tar.gz" "$file"
+	echo "file $filename sudah dikompres dan dipindahkan"
+done
+
+echo "done"
+
+
+
